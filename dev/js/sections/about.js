8e5b19d3056d36ce50bf2a6beb64b9ec86f14ffd
BartoszY <bartosz@yprojects.pl>
2025-04-09 09:24:35 +0200

    Small style changes

--- dev/js/sections/about.js
+++ dev/js/sections/about.js
@@ -5,8 +5,6 @@ const aboutAnimations = () => {
   gsap.registerPlugin(ScrollTrigger);
 
   const aboutElements = document.querySelectorAll(['.about .heading', '.about__text', '.about .button']);
-  const boxLeft = document.querySelector('.about__photo__left');
-  const boxRight = document.querySelector('.about__photo__right');
 
   const tl = gsap.timeline({
     scrollTrigger: {
@@ -19,25 +17,7 @@ const aboutAnimations = () => {
 
   tl
   .from(aboutElements, {opacity: 0, y: 300})
-  .to(aboutElements, {opacity: 1, y: 0})
-  .to(aboutElements, {opacity: 0, y: -300});
-
-
-  // photos animations
-  const photosTl = gsap.timeline({
-    scrollTrigger: {
-      trigger: '.about',
-      start: "-20% top",
-    }
-  });
-
-  photosTl
-  .to(boxLeft, {x: -500})
-  .to(boxRight, {x: 500}, '<')
-  .to(boxLeft, {duration: .1, zIndex:-1})
-  .to(boxRight, {duration: .1, zIndex:-1}, '<')
-  .to(boxLeft, {x: -25})
-  .to(boxRight, {x: 25}, '<');
+  .to(aboutElements, {opacity: 1, y: 0});
 }
 
 export default aboutAnimations;
